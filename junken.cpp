b065d7c692a41a41bfc03e5d163061a74507b7bb
unknown <deathproof.lee@gmail.com>
2019-11-11 10:34:14 -0500

    first commit

---
+++ junken.cpp
@@ -0,0 +1,44 @@
+// This is a janken game!!
+// Takeru Osoegawa
+// 11/11/2019
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	const int 
+	
+	unsigned seed = time(0);
+	srand(seed);
+
+	system("cls");
+	
+	cout << "\n"
+		<< " **********************************************************************\n"
+		<< " *                                                                    *\n"
+		<< " *                            JANKEN GAME                             *\n"
+		<< " *                                                                    *\n"
+		<< " *            This is a typical Rock-Paper-Scissors game.             *\n"
+		<< " *               The game is called Janken in Japanese.               *\n"
+		<< " *                                                                    *\n"
+		<< " *      Rock <1> グー     Paper <2> パー     Scissors <3> チョキ      *\n"
+		<< " *                                                                    *\n"
+		<< " *                      Made by Takeru Osoegawa                       *\n"
+		<< " *                                                                    *\n"
+		<< " **********************************************************************\n";
+	/*	
+	system("pause");
+	system("cls");*/
+
+	cout << "\n"
+		<< "Please enter a number! ";
+	cin >> 
+
+	return 0;
+}
+
+
